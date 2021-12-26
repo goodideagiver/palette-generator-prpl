696cef36572f714a2063dd5e8112a69daf54290d
prplblck <kariurek@gmail.com>
2021-12-26 21:11:20 +0100

    project finished

--- script/script.js
+++ script/script.js
@@ -36,8 +36,8 @@ function componentToHex(c) {
 
 const monobtn = document.getElementById('monobtn');
 monobtn.addEventListener('click',generateMonoSquares)
-const mono = document.getElementById('mono').children;
 function generateMonoSquares() {
+    const mono = document.getElementById('mono').children;
     for (let j = 0; j < mono.length; j++) {
         let color = Math.floor(Math.random()*255);        
         mono[j].style.transition = (mono.length/10)/4 + "s";
@@ -71,7 +71,49 @@ function monoRange() {
     monowrapper.innerHTML = innerMono;
     generateMonoSquares();
     }, ((mono.length/10))*500);
-    
 }
 
 monoRange();
+
+
+const colorbtn = document.getElementById('colorbtn');
+colorbtn.addEventListener('click',generateColorSquares)
+const colorsq = document.getElementById('color').children;
+function generateColorSquares() {
+    for (let j = 0; j < colorsq.length; j++) {
+        let color = Math.floor(Math.random()*255);  
+        let color2 = Math.floor(Math.random()*255);
+        let color3 = Math.floor(Math.random()*255);      
+        colorsq[j].style.transition = (colorsq.length/10)/4 + "s";
+        colorsq[j].style.opacity = 0;
+        colorsq[j].style.transform = "translate(0,0px) rotateX(90deg)";
+        colorsq[j].style.backgroundColor = rgbToHex(color,color2,color3);
+        colorsq[j].style.transitionDelay = ((j+1)/10)/2 + "s";
+        setTimeout(() => {
+            colorsq[j].innerHTML = "<div>" + rgbToHex(color,color2,color3);
+            colorsq[j].style.opacity = 1;
+            colorsq[j].style.transform = "translate(0)";
+        }, ((colorsq.length/10))*1000);
+    }
+}
+
+
+const colorsqwrapper = document.getElementById('color');
+const colorsqrange1 = document.getElementById('colorrange');
+colorsqrange1.addEventListener("click",colorRange)
+
+function colorRange() {
+    for (let j = 0; j < colorsq.length; j++) {
+        colorsq[j].style.opacity = 0;        
+    }
+    setTimeout(() => {
+        let innercolorsq =[]
+    for (let i = 0; i < colorsqrange1.valueAsNumber; i++) {
+       innercolorsq = innercolorsq + "<div class='pal-it'></div>";
+    }
+    colorsqwrapper.innerHTML = innercolorsq;
+    generateColorSquares();
+    }, ((colorsq.length/10))*500);
+}
+
+colorRange();
