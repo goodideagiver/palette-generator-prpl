fafd97c39017c9d52169adaf141d09ec64c891a2
Karol <kariurek@gmail.com>
2022-01-24 08:41:47 +0100

    updated last update date

--- script/script.js
+++ script/script.js
@@ -1,175 +1,183 @@
 function generateMonoPalette() {
-    let colors = [];
-    for (let i = 0; i < 4; i++) {
-        colors[i]=Math.floor(Math.random()*16777215).toString(16);  
-        let loopColor = colors[i];
-        while (loopColor.length < 6) {
-            loopColor = "0" + loopColor;
-        } 
-        colors[i] = loopColor;
-        console.log(colors[i] + " " + colors[i].length);
-    }
+	let colors = [];
+	for (let i = 0; i < 4; i++) {
+		colors[i] = Math.floor(Math.random() * 16777215).toString(16);
+		let loopColor = colors[i];
+		while (loopColor.length < 6) {
+			loopColor = '0' + loopColor;
+		}
+		colors[i] = loopColor;
+		console.log(colors[i] + ' ' + colors[i].length);
+	}
 }
 
 function generateMono() {
-    let colors = [];
-    for (let i = 0; i < 4; i++) {
-        let color = Math.floor(Math.random()*255);
-        colors[i] = rgbToHex(color,color,color)
-    }
-    colors.forEach(element => {
-        console.log(element);
-    });
+	let colors = [];
+	for (let i = 0; i < 4; i++) {
+		let color = Math.floor(Math.random() * 255);
+		colors[i] = rgbToHex(color, color, color);
+	}
+	colors.forEach((element) => {
+		console.log(element);
+	});
 }
 
 function componentToHex(c) {
-    var hex = c.toString(16);
-    return hex.length == 1 ? "0" + hex : hex;
-  }
-  
-  function rgbToHex(r, g, b) {
-    return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
-  }
+	var hex = c.toString(16);
+	return hex.length == 1 ? '0' + hex : hex;
+}
+
+function rgbToHex(r, g, b) {
+	return '#' + componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
 
 const monobtn = document.getElementById('monobtn');
-monobtn.addEventListener('click',generateMonoSquares)
+monobtn.addEventListener('click', generateMonoSquares);
 function generateMonoSquares() {
-    let clrstring = "";
-    const mono = document.getElementById('mono').children;
-    for (let j = 0; j < mono.length; j++) {
-        let color = Math.floor(Math.random()*255);        
-        mono[j].style.transition = (mono.length/10)/4 + "s";
-        mono[j].style.opacity = 0;
-        mono[j].style.transform = "translate(0,0px) rotateX(90deg)";
-        mono[j].style.backgroundColor = rgbToHex(color,color,color);
-        clrstring = clrstring + (" " + rgbToHex(color,color,color));
-        mono[j].style.transitionDelay = ((j+1)/10)/2 + "s";
-        setTimeout(() => {
-            mono[j].innerHTML = "<div>" + rgbToHex(color,color,color);
-            mono[j].style.opacity = 1;
-            mono[j].style.transform = "translate(0)";
-        }, ((mono.length/10))*1000);
-    }
-    document.getElementById('monosum').innerHTML = clrstring;
+	let clrstring = '';
+	const mono = document.getElementById('mono').children;
+	for (let j = 0; j < mono.length; j++) {
+		let color = Math.floor(Math.random() * 255);
+		mono[j].style.transition = mono.length / 10 / 4 + 's';
+		mono[j].style.opacity = 0;
+		mono[j].style.transform = 'translate(0,0px) rotateX(90deg)';
+		mono[j].style.backgroundColor = rgbToHex(color, color, color);
+		clrstring = clrstring + (' ' + rgbToHex(color, color, color));
+		mono[j].style.transitionDelay = (j + 1) / 10 / 2 + 's';
+		setTimeout(() => {
+			mono[j].innerHTML = '<div>' + rgbToHex(color, color, color);
+			mono[j].style.opacity = 1;
+			mono[j].style.transform = 'translate(0)';
+		}, (mono.length / 10) * 1000);
+	}
+	document.getElementById('monosum').innerHTML = clrstring;
 }
 
 const monowrapper = document.getElementById('mono');
 const monorange1 = document.getElementById('monorange');
-monorange1.addEventListener("change",monoRange)
+monorange1.addEventListener('change', monoRange);
 
 function monoRange() {
-    for (let j = 0; j < mono.length; j++) {
-        mono[j].style.opacity = 0;        
-    }
-    setTimeout(() => {
-        let innerMono =[]
-    for (let i = 0; i < monorange1.valueAsNumber; i++) {
-       innerMono = innerMono + "<div class='pal-it'></div>";
-    }
-    monowrapper.innerHTML = innerMono;
-    generateMonoSquares();
-    copySquareColor('#mono .pal-it');
-    }, ((mono.length/10))*500);
+	for (let j = 0; j < mono.length; j++) {
+		mono[j].style.opacity = 0;
+	}
+	setTimeout(() => {
+		let innerMono = [];
+		for (let i = 0; i < monorange1.valueAsNumber; i++) {
+			innerMono = innerMono + "<div class='pal-it'></div>";
+		}
+		monowrapper.innerHTML = innerMono;
+		generateMonoSquares();
+		copySquareColor('#mono .pal-it');
+	}, (mono.length / 10) * 500);
 }
 
 monoRange();
 
 const colorbtn = document.getElementById('colorbtn');
-colorbtn.addEventListener('click',generateColorSquares)
+colorbtn.addEventListener('click', generateColorSquares);
 const colorsq = document.getElementById('color').children;
 function generateColorSquares() {
-    clrstring = "";
-    for (let j = 0; j < colorsq.length; j++) {
-        let color = Math.floor(Math.random()*255);  
-        let color2 = Math.floor(Math.random()*255);
-        let color3 = Math.floor(Math.random()*255);      
-        clrstring = clrstring + (" " + rgbToHex(color,color2,color3));
-        colorsq[j].style.transition = (colorsq.length/10)/4 + "s";
-        colorsq[j].style.opacity = 0;
-        colorsq[j].style.transform = "translate(0,0px) rotateX(90deg)";
-        colorsq[j].style.backgroundColor = rgbToHex(color,color2,color3);
-        colorsq[j].style.transitionDelay = ((j+1)/10)/2 + "s";
-        setTimeout(() => {
-            colorsq[j].innerHTML = "<div>" + rgbToHex(color,color2,color3);
-            colorsq[j].style.opacity = 1;
-            colorsq[j].style.transform = "translate(0)";
-        }, ((colorsq.length/10))*1000);
-    }
-    document.getElementById('colorsum').innerHTML = clrstring;
+	clrstring = '';
+	for (let j = 0; j < colorsq.length; j++) {
+		let color = Math.floor(Math.random() * 255);
+		let color2 = Math.floor(Math.random() * 255);
+		let color3 = Math.floor(Math.random() * 255);
+		clrstring = clrstring + (' ' + rgbToHex(color, color2, color3));
+		colorsq[j].style.transition = colorsq.length / 10 / 4 + 's';
+		colorsq[j].style.opacity = 0;
+		colorsq[j].style.transform = 'translate(0,0px) rotateX(90deg)';
+		colorsq[j].style.backgroundColor = rgbToHex(color, color2, color3);
+		colorsq[j].style.transitionDelay = (j + 1) / 10 / 2 + 's';
+		setTimeout(() => {
+			colorsq[j].innerHTML = '<div>' + rgbToHex(color, color2, color3);
+			colorsq[j].style.opacity = 1;
+			colorsq[j].style.transform = 'translate(0)';
+		}, (colorsq.length / 10) * 1000);
+	}
+	document.getElementById('colorsum').innerHTML = clrstring;
 }
 
 const colorsqwrapper = document.getElementById('color');
 const colorsqrange1 = document.getElementById('colorrange');
-colorsqrange1.addEventListener("change",colorRange)
+colorsqrange1.addEventListener('change', colorRange);
 
 function colorRange() {
-    for (let j = 0; j < colorsq.length; j++) {
-        colorsq[j].style.opacity = 0;        
-    }
-    setTimeout(() => {
-        let innercolorsq =[]
-    for (let i = 0; i < colorsqrange1.valueAsNumber; i++) {
-       innercolorsq = innercolorsq + "<div class='pal-it'></div>";
-    }
-    colorsqwrapper.innerHTML = innercolorsq;
-    generateColorSquares();
-    copySquareColor('#color .pal-it');
-    }, ((colorsq.length/10))*500);
+	for (let j = 0; j < colorsq.length; j++) {
+		colorsq[j].style.opacity = 0;
+	}
+	setTimeout(() => {
+		let innercolorsq = [];
+		for (let i = 0; i < colorsqrange1.valueAsNumber; i++) {
+			innercolorsq = innercolorsq + "<div class='pal-it'></div>";
+		}
+		colorsqwrapper.innerHTML = innercolorsq;
+		generateColorSquares();
+		copySquareColor('#color .pal-it');
+	}, (colorsq.length / 10) * 500);
 }
 
 colorRange();
 
 function copyColors(text) {
-    navigator.clipboard.writeText(text).then(function() {
-        showMessage('Copied ' + text + '! 😄');
-      }, function(err) {
-        showMessage('Could not copy to clipboard 😢');
-      });
+	navigator.clipboard.writeText(text).then(
+		function () {
+			showMessage('Copied ' + text + '! 😄');
+		},
+		function (err) {
+			showMessage('Could not copy to clipboard 😢');
+		}
+	);
 }
 
 const notification = document.getElementById('notification');
 const notifWrapper = document.querySelector('.notif-wrapper');
 function showMessage(message) {
-    const notification = document.createElement('div');
-    notification.classList.add('disabled','notification');
-    notification.innerText = message;
-    notifWrapper.appendChild(notification);
-    setTimeout(() => {
-        notification.classList.remove('disabled');
-        setTimeout(() => {
-            notification.classList.add('disabled');
-            setTimeout(() => {
-                notification.remove();
-            }, 300);
-        }, 2000);
-    }, 20);
+	const notification = document.createElement('div');
+	notification.classList.add('disabled', 'notification');
+	notification.innerText = message;
+	notifWrapper.appendChild(notification);
+	setTimeout(() => {
+		notification.classList.remove('disabled');
+		setTimeout(() => {
+			notification.classList.add('disabled');
+			setTimeout(() => {
+				notification.remove();
+			}, 300);
+		}, 2000);
+	}, 20);
 }
 
-document.getElementById('monosum').addEventListener('click',copyMono);
+document.getElementById('monosum').addEventListener('click', copyMono);
 function copyMono() {
-    Colors = document.getElementById('monosum').innerText;
-    copyColors(Colors);
+	Colors = document.getElementById('monosum').innerText;
+	copyColors(Colors);
 }
 
-document.getElementById('colorsum').addEventListener('click',copyColor);
+document.getElementById('colorsum').addEventListener('click', copyColor);
 function copyColor() {
-    Colors = document.getElementById('colorsum').innerText;
-    copyColors(Colors);
+	Colors = document.getElementById('colorsum').innerText;
+	copyColors(Colors);
 }
 
 function copySquareColor(select) {
-    let allSquares = document.querySelectorAll(select);
-    allSquares.forEach(Square => {
-        Square.addEventListener('click',() => copyColors(Square.innerText))
-    });
+	let allSquares = document.querySelectorAll(select);
+	allSquares.forEach((Square) => {
+		Square.addEventListener('click', () => copyColors(Square.innerText));
+	});
 }
 
-
 const lastUpdate = document.getElementById('lastUpdate');
-fetch('https://api.github.com/repos/goodideagiver/palette-generator-prpl')
-.then(response => response.json())
-.then(data => {
-  let updateDate = new Date(data.updated_at);
-  lastUpdate.innerText = 'Last updated: ' + updateDate.toLocaleDateString() + ' ' + updateDate.toLocaleTimeString();
-})
-.catch(error => lastUpdate.innerText = '');
+fetch(
+	'https://api.github.com/repos/goodideagiver/palette-generator-prpl/commits'
+)
+	.then((response) => response.json())
+	.then((data) => {
+		let updateDate = new Date(data[0].commit.committer.date);
+		lastUpdate.innerText =
+			'Last updated: ' +
+			updateDate.toLocaleDateString() +
+			' ' +
+			updateDate.toLocaleTimeString();
+	})
+	.catch((error) => (lastUpdate.innerText = ''));
